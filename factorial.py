6af866e7ee07efb9bcfd2ecc77c85d63e16fea3e
RamyadeviSivakumar <sivakumarvengadesan@gmail.com>
2025-02-06 18:43:03 +0530

    Modified

--- factorial.py
+++ factorial.py
@@ -3,8 +3,8 @@ def fact(n):
     return 1
   else:
     res=1
-    for i in range(n,n+1):
-      res*=n-1
+    for i in range(1,n+1):
+      res*=i
     return res
 
 num=int(input("Enter the number:"))
